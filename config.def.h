a94a201e96e02ee81c95631387282767a0d9ce5c
Artjom Löbsack <me@ceigh.com>
2020-11-10 23:39:42 +0500

    feat: add backup bold font

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Iosevka Nerd Font:style=Medium:pixelsize=16:antialias=true:autohint=true";
+// static char *font = "Iosevka Nerd Font:style=Bold:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
